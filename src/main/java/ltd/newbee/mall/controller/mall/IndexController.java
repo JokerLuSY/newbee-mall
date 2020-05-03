0d3094e6696ffed68f61810daad2acdb7f92ce9b
Joker <lushengyihaha7322@163.com>
2020-05-03 16:06:32 +0800

    Mall issue1

--- src/main/java/ltd/newbee/mall/controller/mall/IndexController.java
+++ src/main/java/ltd/newbee/mall/controller/mall/IndexController.java
@@ -7,14 +7,20 @@ import ltd.newbee.mall.controller.vo.NewBeeMallIndexCategoryVO;
 import ltd.newbee.mall.controller.vo.NewBeeMallIndexConfigGoodsVO;
 import ltd.newbee.mall.service.NewBeeMallCarouselService;
 import ltd.newbee.mall.service.NewBeeMallCategoryService;
+import ltd.newbee.mall.service.NewBeeMallGoodsService;
 import ltd.newbee.mall.service.NewBeeMallIndexConfigService;
+import ltd.newbee.mall.util.PageQueryUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -27,13 +33,12 @@ public class IndexController {
 
     @Resource
     private NewBeeMallCategoryService newBeeMallCategoryService;
+    @Resource
+    private NewBeeMallGoodsService newBeeMallGoodsService;
 
     @GetMapping({"/index", "/", "/index.html"})
     public String indexPage(HttpServletRequest request) {
         List<NewBeeMallIndexCategoryVO> categories = newBeeMallCategoryService.getCategoriesForIndex();
-        if (CollectionUtils.isEmpty(categories)) {
-            return "error/error_5xx";
-        }
         List<NewBeeMallIndexCarouselVO> carousels = newBeeMallCarouselService.getCarouselsForIndex(Constants.INDEX_CAROUSEL_NUMBER);
         List<NewBeeMallIndexConfigGoodsVO> hotGoodses = newBeeMallIndexConfigService.getConfigGoodsesForIndex(IndexConfigTypeEnum.INDEX_GOODS_HOT.getType(), Constants.INDEX_GOODS_HOT_NUMBER);
         List<NewBeeMallIndexConfigGoodsVO> newGoodses = newBeeMallIndexConfigService.getConfigGoodsesForIndex(IndexConfigTypeEnum.INDEX_GOODS_NEW.getType(), Constants.INDEX_GOODS_NEW_NUMBER);
@@ -43,6 +48,23 @@ public class IndexController {
         request.setAttribute("hotGoodses", hotGoodses);//热销商品
         request.setAttribute("newGoodses", newGoodses);//新品
         request.setAttribute("recommendGoodses", recommendGoodses);//推荐商品
+        try
+        {
+            if (CollectionUtils.isEmpty(categories)) {
+                return "error/error_5xx";
+            }
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("keyword", "");
+            params.put("page", 1);
+            params.put("limit", 100);
+            PageQueryUtil pageUtil = new PageQueryUtil(params);
+            request.setAttribute("pageResult", newBeeMallGoodsService.searchNewBeeMallGoods(pageUtil));
+        }catch(Exception ex)
+        {
+
+        }
         return "mall/index";
+
+
     }
 }
